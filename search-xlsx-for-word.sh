34092fca84a715c720bde876d89388deff62173b
pvbemmelen62 <pvbemmelen62@gmail.com>
2022-09-09 13:16:58 +0200

    Handle "jar xvf" being unsuccessful.
    Handle "jar xvf" being unsuccessful , and added --help option.

--- search-xlsx-for-word.sh
+++ search-xlsx-for-word.sh
@@ -7,6 +7,13 @@ Copies each file into a tmp/excel.jar,
 changes dir into tmp, extracts excel.jar, and then searches xl/sharedStrings.xml .
 Afterwards changes one dir up, and deletes subdir tmp ."
 
+if [ $# -ge 1 ]; then
+  if [ "$1" == "--help" ] ; then
+    echo "$USAGE" 1>&2
+    exit 0
+  fi
+fi
+
 if [ $# -le 2 ]; then
   echo "$USAGE" 1>&2
   exit 1
@@ -37,11 +44,15 @@ for i in "$@" ; do
   mkdir tmp
   cp "$i" tmp/excel.jar
   cd tmp > /dev/null
-  jar xvf excel.jar > /dev/null
-  lines=`cat xl/sharedStrings.xml | xmllint --format - | grep $word`
-  if [ "" != "$lines" ] ; then
-    echo "$i"
-    echo "$lines"
+  jar xvf excel.jar > /dev/null 2>&1
+  if [ $? == 0 ]; then
+    lines=`cat xl/sharedStrings.xml | xmllint --format - | grep $word`
+    if [ "" != "$lines" ] ; then
+      echo "$i"
+      echo "$lines"
+    fi
+  else
+    echo "Can't extract; skipping file: $i"
   fi
   cd .. > /dev/null
   rm -rf tmp
